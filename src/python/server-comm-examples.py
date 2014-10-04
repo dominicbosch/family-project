63a423aafeab7f6b45a96c5c426bbbe6ff3d3135
Peter Bosch <pitvb56@outlook.com>
2014-10-04 15:40:11 +0200

    Wrong Version

--- src/python/server-comm-examples.py
+++ src/python/server-comm-examples.py
@@ -11,6 +11,6 @@
 
 #PYTHON:
 import httplib2
-conn = httplib2.HTTPConnection("http://192.168.0.79:8083/")
+# conn = httplib2.HTTPConnection("http://192.168.0.79:8083/")
 # conn.request("GET", "/index.html")
 conn.request("GET", "/ZWaveAPI/Run/devices[2].instances[0].commandClasses[0x25].Set(1)")
