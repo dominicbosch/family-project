ed2b700ee659d5d4f97eae55adcfe923b0691e41
Peter Bosch <pitvb56@outlook.com>
2014-10-04 15:50:16 +0200

    ssh test

--- src/python/server-comm-examples.py
+++ src/python/server-comm-examples.py
@@ -31,3 +31,4 @@ print(mydict['value'])
 #conn = httplib2.Http("http://192.168.0.79:8083/")
 # conn.request("GET", "/index.html")
 #conn.request("GET", "/ZWaveAPI/Run/devices[2].instances[0].commandClasses[0x25].Set(0)")
+#test
