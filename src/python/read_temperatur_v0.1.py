b06458b0d88dbb704c918eb3267a65ef525a6bb0
Peter Bosch <pitvb56@outlook.com>
2014-11-23 20:09:39 +0100

    Temperature reader V0.1

--- src/python/read_temperatur_v0.1.py
+++ src/python/read_temperatur_v0.1.py
@@ -1,13 +1,10 @@
-#Straigth copy from read switches
-#Work in progress
-
 import time
 import httplib2
 import json
 
 # Conifguration Record,
 # Device Id (Assigned manually)
-# Device Type (T = Temperatur Sensor)
+# Device Type (S = Switch, T = Temp sensor)
 # Device Number (Assigned by Server)
 # Device Instance
 # Device Name
@@ -18,40 +15,34 @@ iSleepTime = 10
 #sStopFileName = "/mnt/nas/stop.yes"
 #sDeviceConfigFileName = "config.txt"
 
-sFileName = "switch.inf"
+sFileName = "temp.inf"
 sDeviceConfigFileName = "config.txt"
 sStopFileName = "stop.yes"
 sServerAddress = "192.168.0.79:8083"
 
 h = httplib2.Http()
 
+
 def compile_httprefresh(iDeviceNum, iDeviceInstance):
     sOutBuf = "http://" + sServerAddress + "/ZWaveAPI/Run/devices[" + str(iDeviceNum)
     sOutBuf = sOutBuf + "].instances[" + str(iDeviceInstance) + "].commandClasses[0x25].Get()"
     return sOutBuf
 
-def compile_httpgetstat(iDeviceNum, iDeviceInstance):
-    sOutBuf = "http://" + sServerAddress + "/ZWaveAPI/Run/devices[" + str(iDeviceNum)
-    sOutBuf = sOutBuf + "].instances[" + str(iDeviceInstance) + "].commandClasses[0x25].data.level"
+def compile_httpgettemp(IDeviceNum, ICmdClass):
+    sOutBuf = "http://192.168.0.79:8083/ZWaveAPI/Run/devices[" + str(IDeviceNum)
+    sOutBuf = sOutBuf + "].instances[0].commandClasses[" + str(ICmdClass)
+    sOutBuf = sOutBuf + "].data[1].val.value"
     return sOutBuf
 
-def get_currentstat(iDeviceNum, iDeviceInstance):
+def get_currenttemp(iDeviceNum, iDeviceInstance):
     resp, content = h.request(compile_httprefresh(iDeviceNum, iDeviceInstance), "GET")
-    resp, content = h.request(compile_httpgetstat(iDeviceNum, iDeviceInstance), "GET")
+    resp, content = h.request(compile_httpgettemp(iDeviceNum, 49), "GET")
     try:
         DDevInfo = json.loads(content.decode('ascii'))
         return DDevInfo
     except (ValueError):
         return ""
-
-def get_switchstat(DDevInfo):
-    BDevStatus = DDevInfo['value']
-    if BDevStatus == True:
-        SDevStatus = "On"
-    else:
-        SDevStatus = "Off"
-    return SDevStatus
-
+    
 def get_currenttime(DDevInfo):
     return DDevInfo["updateTime"]
 
@@ -82,16 +73,16 @@ while StopRun == False:
         iDeviceInst = int(rConfigRecord[3])
         sDeviceName = rConfigRecord[4].strip()
 
-        sDevResp = get_currentstat(iDeviceNum, iDeviceInst)
-        if sDevResp != "":
-            sDevStatus = get_switchstat(sDevResp)
-            lRequestTime = get_currenttime(sDevResp)
-            sRequestTime = time.ctime(lRequestTime)
-            print("Device :", str(iDeviceNum), ":", sDeviceName, "- Time :", sRequestTime, "- Status :", sDevStatus, "\n")
-            fObiS.write("{}, {}, {}\n".format(str(iDeviceNum), lRequestTime, sDevStatus))
-        else:
-            print("Value Error thrown - ", time.ctime(time.time()))
-            fObiS.write("{} {}\n".format("Value erroro at :", time.ctime(time.time())))
+        if sDeviceType == "T":
+            sDevResp = get_currenttemp(iDeviceNum, iDeviceInst)
+            if sDevResp != "":
+                sDevTemp = sDevResp
+                sRequestTime = time.ctime(time.time())
+                print("Device :", str(iDeviceNum), ":", sDeviceName, "- Time :", sRequestTime, "- Temparatur : ", sDevTemp)
+                fObiS.write("{}, {}, {}\n".format(str(iDeviceNum), sRequestTime, sDevTemp))
+            else:
+                print("Value Error thrown - ", time.ctime(time.time()))
+                fObiS.write("{} {}\n".format("Value erroro at :", time.ctime(time.time())))
 
     fObiS.close()
     fConfFile.close()
