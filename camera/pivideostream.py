e3fdef7e93c96b0ce4f2b20b21a75eb17920e82e
Dominic Bosch <dominic.bosch@dobotech.ch>
2016-10-09 20:16:09 +0200

    Don't flip horizontally

--- camera/pivideostream.py
+++ camera/pivideostream.py
@@ -5,7 +5,7 @@ from threading import Thread
 class PiVideoStream:
 	def __init__(self, resolution=(1024, 768), framerate=32, hflip=False, vflip=False):
 		# initialize the camera and stream
-		print 'Starting camera with hflip={}, vflip={}, resolution={}, framerate={}'.format(hflip, vflip, '|'.join(resolution), framerate)
+		print 'Starting camera with hflip={}, vflip={}, resolution={}x{}, framerate={}'.format(hflip, vflip, resolution[0], resolution[1], framerate)
 		self.camera = PiCamera()
 		self.camera.resolution = resolution
 		self.camera.framerate = framerate
