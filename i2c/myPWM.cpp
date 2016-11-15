2e5b8164076ee3746bd0e666283f03f8f5b021a2
PitvB <pi@raspberrypi.(none)>
2016-11-15 18:57:17 +0100

    Two Servo move - slow move

--- i2c/myPWM.cpp
+++ i2c/myPWM.cpp
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
+#include <stdlib.h>
+//#include <dos.h>
 
 int pwmHatFD = -1;
 int servoMin = 150;
-int servoMax = 600;
+int servoMax = 560;
+int servoMid = 350;
+int hServoMid = 300;
+
 
 void initPWM(int address)
 {
@@ -60,18 +65,70 @@ void resetAllPWM(int on, int off)
     wiringPiI2CWriteReg8(pwmHatFD, __ALL_LED_OFF_H, off >> 8);
 }
 
+void moveSlow(int sNum, int curPos, int toPos, int mSpeed)
+{
+
+    int loopCount = curPos;
+
+    if(curPos >= toPos)
+	{
+	while(loopCount >= toPos)
+		{
+		setPWM(sNum, 0, loopCount);
+		loopCount = loopCount -1;
+		usleep(5000);
+		}
+	}
+    else
+	{
+	while (toPos >= loopCount)
+		{
+		setPWM(sNum, 0, loopCount);
+		loopCount = loopCount+1;
+		usleep(5000);
+		}
+	} 
+
+}
+
 int main()
 {
+
+int hOldPos;
+
 printf("Init PWM\n");
 initPWM(0x40);
+
+printf("Set Servo 1 to mid\n");
+setPWM(0, 0, servoMid);
+hOldPos = servoMid;
+
+printf("Set Servo 2 to mid\n");
+setPWM(1, 0, hServoMid);
+
+printf("Move Servo 1 to min\n");
+moveSlow(0, hOldPos, servoMin, 1);
+hOldPos = servoMin;
 sleep(1);
 
-printf("Set Servo to min\n");
-setPWM(0, 0, servoMin);
+printf("Set Servo 2 to min\n");
+setPWM(1, 0, servoMin);
 sleep(1);
 
-printf("Set Servo to max\n");
-setPWM(0, 0, servoMax);
+printf("Move Servo 1 to max\n");
+moveSlow(0, hOldPos, servoMax, 1);
+hOldPos = servoMax;
+sleep(1);
+
+printf("Set Servo 2 to max\n");
+setPWM(1, 0, servoMax);
+sleep(1);
+
+printf("Move Servo 1 to mid\n");
+moveSlow(0, hOldPos, servoMid, 1);
+
+printf("Set Servo 2 to mid\n");
+setPWM(1, 0, hServoMid);
 
 return 0;
 }
