bc7b5cadd0b7e290125d43846c53026f3ba7db81
Peter <pitvb56@outlook.com>
2016-12-08 20:50:07 +0100

    myConvert ... still core dump if device not exists

--- i2c/myConvert.cpp
+++ i2c/myConvert.cpp
@@ -5,8 +5,9 @@
 //#include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libconfig.h>
 
-double dLimitArray[] = {200, 325, 500, 0, 0};
+double dLimitArray[] = {0, 0, 0, 0, 0};
 
 bool bCalcArray(double *dArray)
 {
@@ -49,17 +50,23 @@ int strToint(char *inString)
 int main(int argc, char **argv)
 {
 
-int iResult, i;
+	config_t cfg, *cf;
+	config_setting_t *iInVal = 0;
 
-int iOut[3];
-int iRetval;
-int iArg;
+	char sFChar;
+//	char sSelVal[6];
 
-bool bRetval;
+	int iResult, i;
 
+	int iOut[3];
+	int iRetval;
+	int iArg;
+	int iChar;
 
-	if (argc <= 1) {
-		printf("Supply a commands to send to the Arduino\n");
+	bool bRetval;
+
+	if (argc <= 3) {
+		printf("Supply  3 commands\n");
 		exit(1);
 		}
 
@@ -70,17 +77,45 @@ bool bRetval;
 
 	printf("iOut[0] = %i\n", iOut[0]);
 
+	cf = &cfg;
+    	config_init(cf);
+
+	if (!config_read_file(cf, "carconfig.ini")) 
+	{
+        	fprintf(stderr, "%s:%d - %s\n",
+            	config_error_file(cf),
+            	config_error_line(cf),
+            	config_error_text(cf));
+        	config_destroy(cf);
+        	return(EXIT_FAILURE);
+    	}
+
+	char sMinVal[] = "9.min";
+	sMinVal[0]= char(97 + iOut[0]);
+	iInVal = config_lookup(cf, sMinVal); 
+	dLimitArray[0] = config_setting_get_int(iInVal);
+
+	char sMidVal[] = "9.mid";
+	sMidVal[0]= char(97 + iOut[0]);
+	iInVal = config_lookup(cf, sMidVal); 
+	dLimitArray[1] = config_setting_get_int(iInVal);
+
+	char sMaxVal[] = "9.max";
+	sMaxVal[0]= char(97 + iOut[0]);
+	iInVal = config_lookup(cf, sMaxVal); 
+	dLimitArray[2] = config_setting_get_int(iInVal);
+
 	bRetval = bCalcArray(dLimitArray);
 
-	if(iOut[0] <= -1)
+	if(iOut[1] <= -1)
 		{
-		iResult = dLimitArray[1] + (dLimitArray[3] * iOut[0]);
+		iResult = dLimitArray[1] + (dLimitArray[3] * iOut[1]);
 		}
 	else
 		{
-		if(iOut[0] >= 1)
+		if(iOut[1] >= 1)
 			{
-			iResult = dLimitArray[1] + (dLimitArray[4] * iOut[0]);
+			iResult = dLimitArray[1] + (dLimitArray[4] * iOut[1]);
 			}
 		else
 			{
