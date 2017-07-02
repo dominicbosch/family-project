4f6166cc0e3157325a9fa35df0f0900b8404ed31
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-07-02 22:30:21 +0200

    ultrasonic update

--- ultrasonic/detectobstacle.py
+++ ultrasonic/detectobstacle.py
@@ -1,5 +1,7 @@
 from time import sleep
+from threading import Thread
 from gpiozero import DistanceSensor
+
 
 class DetectObstacle:
 	def __init__(self, callback, pinTrigger=4, pinEcho=17, maxDist=2, detectThresh=1.0, wait=0.5):
@@ -16,12 +18,12 @@ class DetectObstacle:
 			threshold_distance=detectThresh
 		)
 
-		self.ultrasonic.when_in_range = self.__detectedObstacle
-		self.ultrasonic.when_out_of_range = self.__noMoreObstacle
+		# self.ultrasonic.when_in_range = self.__detectedObstacle
+		# self.ultrasonic.when_out_of_range = self.__noMoreObstacle
+		Thread(target=self.__update, args=()).start()
 		print('Ultrasonic device started...')
 
-	def __detectedObstacle(self):
-		print("Obstacle at %.1f " % self.ultrasonic.distance)
+	def __update(self):
 		self.isRunning = True
 		while self.isRunning:
 			dist = self.ultrasonic.distance
@@ -29,9 +31,18 @@ class DetectObstacle:
 			self.callback(dist)
 			sleep(self.wait)
 
-	def __noMoreObstacle(self):
-		self.isRunning = False
-		print("Out of range at %.1f " % self.ultrasonic.distance)
+	# def __detectedObstacle(self):
+	# 	print("Obstacle at %.1f " % self.ultrasonic.distance)
+	# 	self.isRunning = True
+	# 	while self.isRunning:
+	# 		dist = self.ultrasonic.distance
+	# 		print("... Updated to: %.1f " % dist)
+	# 		self.callback(dist)
+	# 		sleep(self.wait)
+
+	# def __noMoreObstacle(self):
+	# 	self.isRunning = False
+	# 	print("Out of range at %.1f " % self.ultrasonic.distance)
 
 	def exit(self):
 		self.isRunning = False
