ce080077c4b229f18160f9edfc337f8ff002e3d4
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-07-02 22:17:12 +0200

    ultrasonic update

--- examples/testNonBlockingUltrasonic.py
+++ examples/testNonBlockingUltrasonic.py
@@ -1,10 +1,15 @@
 import sys
 sys.path.insert(0, '../ultrasonic')
 
+from time import sleep
 from detectobstacle import DetectObstacle
+
 
 def callback(dist):
 	print("Callback distance: %.1f" % dist)
 
-detector = DetectObstacle(1)
-# detector = DetectObstacle(callback)
+detector = DetectObstacle(callback)
+
+# we pretend to be a very busy process:
+while True:
+	sleep(0.01)
