6e26c0f66bbb88c05c8b1bdfbb6bb81d5fce43b9
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-07-15 20:26:49 +0200

    forgot to commit

--- autonomous-systems/carController.js
+++ autonomous-systems/carController.js
@@ -48,7 +48,7 @@ exports.init = function(config) {
 	pyFaces.on('error', function(d) { console.log('pythonFaces Error: ', d) });
 	pyFaces.on('fps', function(d) { emitEvent('camerafps', d) });
 	pyFaces.on('face', handleNewFace);
-	pyFaces.on('detecttime', function(d) { emitEvent('detectfps', 1/d) });
+	pyFaces.on('detectfps', function(d) { emitEvent('detectfps', d) });
 	pyFaces.on('storedimage', function(d) { emitEvent('storedimage', d) });
 	pyFaces.on('storedface', function(d) { emitEvent('storedface', d) });
 
