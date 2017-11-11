47f35a25eda48b17c43071d88e79f2cb08bf40a6
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-11-11 11:36:49 +0100

    timestamp and initial invocation added

--- tempAndHumiLogger/index.js
+++ tempAndHumiLogger/index.js
@@ -6,18 +6,24 @@ const conf = [
 	{ pin: 12, type: 22 }
 ];
 
-setInterval(function() {
+
+function runAllSensors() {
 	for (let i = 0; i < conf.length; i++) {
 		readSensorAndStore(conf[i].pin, conf[i].type);
 	}
-}, 10000);
+}
 
 function readSensorAndStore(pin, type) {
 	sensor.read(pin, type, function(err, temp, humi) {
 		if (!err) {
 			temp = temp.toFixed(2);
 			humi = humi.toFixed(2);
-			fs.appendFileSync('data_pin'+pin+'_type'+type+'csv', temp+','+humi);
+			let ts = (new Date()).getTime();
+			let name = 'data_pin'+pin+'_type'+type+'.csv';
+			fs.appendFileSync(name, ts+','+temp+','+humi);
 		}
 	});
 }
+// Do it initially and then every ten seconds
+runAllSensors();
+setInterval(runAllSensors, 10000);
