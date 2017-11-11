d3ce3ee13ae8ac550f9b018591f9edc15462d5cd
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-11-11 11:25:45 +0100

    3 sensors attached

--- tempAndHumiLogger/index.js
+++ tempAndHumiLogger/index.js
@@ -1,9 +1,23 @@
 const sensor = require('node-dht-sensor');
 
-sensor.read(22, 4, function(err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-    }
-});
+const conf = [
+	{ pin: 5, type: 11 },
+	{ pin: 6, type: 11 },
+	{ pin: 12, type: 22 }
+];
+
+setInterval(function() {
+	for (let i = 0; i < conf.length; i++) {
+		readSensorAndStore(conf[i].pin, conf[i].type);
+	}
+}, 10000);
+
+function readSensorAndStore(pin, type) {
+	sensor.read(pin, type, function(err, temp, humi) {
+		if (!err) {
+			temp = temp.toFixed(2);
+			humi = humi.toFixed(2);
+			fs.appendFileSync('data_pin'+pin+'_type'+type+'csv', temp+','+humi);
+		}
+	});
+}
