78ff8cfac317cc6f98705613a026941a127814f9
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-11-12 21:24:58 +0100

    basic webpage added

--- tempAndHumiLogger/index.js
+++ tempAndHumiLogger/index.js
@@ -1,30 +1,89 @@
 const fs = require('fs');
 const sensor = require('node-dht-sensor');
+const http = require('http');
+const WebSocket = require('ws');
+const express = require('express');
 
 const conf = [
-	{ pin: 5, type: 11 },
-	{ pin: 6, type: 11 },
-	{ pin: 12, type: 22 }
+	{ pin: 5, type: 11, id: 'DHT11 #1' },
+	{ pin: 6, type: 11, id: 'DHT11 #2' },
+	{ pin: 12, type: 22, id: 'AM2302' }
 ];
+const currVals = {};
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static('www'));
+
+// this will be quite an intensive task once the logs get bigger:
+app.get('/getlogs', function(req, res){
+	let start = process.hrtime();
+	Promise.all(conf.map(d => fetchLog(d)))
+		.then((arr) => {
+			console.log('Log fetching took %d', start-process.hrtime())
+			res.send(arr);
+		});
+});
+
+// Standard reply to all nonsense:
+app.use(function (req, res) {
+	res.send(currVals);
+});
+
+// if new client connects to socket he immediately receives the latest data
+wss.on('connection', function(ws, req) {
+	ws.send(JSON.stringify(currVals));
+});
+
+function dataLogPath(sens) {
+	return __dirname+'/datalogs/data_pin'+sens.pin+'_type'+sens.type+'.csv';
+}
+
+function fetchLog(sens) {
+	return newPromise(function(resolve, reject) {
+		fs.readFile(dataLogPath(sens), (err, data) => {
+			if(err) reject(err);
+			else resolve({
+				sensor: sens,
+				data: JSON.parse(data)
+			});
+		})
+	});
+}
 
 function runAllSensors() {
 	for (let i = 0; i < conf.length; i++) {
-		readSensorAndStore(conf[i].pin, conf[i].type);
+		readSensorAndStore(conf[i]);
 	}
 }
 
-function readSensorAndStore(pin, type) {
-	sensor.read(type, pin, function(err, temp, humi) {
+function readSensorAndStore(sens) {
+	sensor.read(sens.type, sens.pin, function(err, temp, humi) {
 		if (!err) {
 			temp = temp.toFixed(2);
 			humi = humi.toFixed(2);
 			let ts = (new Date()).getTime();
-			let name = __dirname+'/datalogs/data_pin'+pin+'_type'+type+'.csv';
-			fs.appendFileSync(name, ts+','+temp+','+humi);
+			currVals[sens.id] = {
+				temp: temp,
+				humi: humi,
+				ts: ts
+			};
+			wss.clients.forEach(function(client) {
+				client.send(currVals);
+			});
+			fs.appendFileSync(dataLogPath(sens), ts+','+temp+','+humi);
 		}
 	});
 }
+
+
+
+server.listen(8080, function listening() {
+	console.log('Listening on %d', server.address().port);
+});
+
 // Do it initially and then every ten seconds
 runAllSensors();
 setInterval(runAllSensors, 10000);
---
+++ tempAndHumiLogger/www/js/code.js
@@ -0,0 +1,53 @@
+document.addEventListener('load', function() {
+	fetchData(window.location.host+'/getlog')
+		.then(visualizeLog);
+});
+
+function fetchData(url) {
+	return new Promise(function(resolve, reject) {
+		// Yay let's do it the real way!
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				if (xhr.status === 200) {
+					resolve(JSON.parse(xhr.responseText));
+				} else {
+					reject(new Error(xhr.status));
+				}
+			}
+		};
+		xhr.open('GET', url, true);
+		xhr.send();
+	});
+}
+
+let socket = new WebSocket('ws://'+window.location.host);
+console.log('Connecting socket to '+'ws://'+window.location.host)
+
+socket.onmessage = function (event) {
+	console.log('realtime measurement', event.data);
+	updateRealtimeMeasurements(JSON.parse(event.data));
+};
+
+socket.onerror = function (err) {
+	console.error(err);
+};
+
+socket.onclose = function (evt) {
+	console.log('socket closed', evt);
+};
+
+function updateRealtimeMeasurements(data) {
+	document.querySelector('#temp td:nth-child(2)').innerHTML = data['AM2302'].temp;
+	document.querySelector('#temp td:nth-child(3)').innerHTML = data['DHT11 #1'].temp;
+	document.querySelector('#temp td:nth-child(4)').innerHTML = data['DHT11 #2'].temp;
+	document.querySelector('#humi td:nth-child(2)').innerHTML = data['AM2302'].humi;
+	document.querySelector('#humi td:nth-child(3)').innerHTML = data['DHT11 #1'].humi;
+	document.querySelector('#humi td:nth-child(4)').innerHTML = data['DHT11 #2'].humi;
+}
+
+
+
+function visualizeLog(data) {
+	console.log('comeon you lazy guy, implement the visualization', data);
+}
