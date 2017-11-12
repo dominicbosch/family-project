d1edc31779e518c798887c8fbf5b3e697f11b143
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-11-12 21:49:46 +0100

    bugfixing static serving

--- tempAndHumiLogger/index.js
+++ tempAndHumiLogger/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const sensor = require('node-dht-sensor');
+// const sensor = require('node-dht-sensor');
 const http = require('http');
 const WebSocket = require('ws');
 const express = require('express');
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-app.use(express.static('www'));
+app.use('/', express.static(__dirname+'/www'));
 
 // this will be quite an intensive task once the logs get bigger:
 app.get('/getlogs', function(req, res){
@@ -28,9 +28,9 @@ app.get('/getlogs', function(req, res){
 });
 
 // Standard reply to all nonsense:
-app.use(function (req, res) {
-	res.send(currVals);
-});
+// app.use(function (req, res) {
+// 	res.send(currVals);
+// });
 
 // if new client connects to socket he immediately receives the latest data
 wss.on('connection', function(ws, req) {
@@ -60,22 +60,22 @@ function runAllSensors() {
 }
 
 function readSensorAndStore(sens) {
-	sensor.read(sens.type, sens.pin, function(err, temp, humi) {
-		if (!err) {
-			temp = temp.toFixed(2);
-			humi = humi.toFixed(2);
-			let ts = (new Date()).getTime();
-			currVals[sens.id] = {
-				temp: temp,
-				humi: humi,
-				ts: ts
-			};
-			wss.clients.forEach(function(client) {
-				client.send(currVals);
-			});
-			fs.appendFileSync(dataLogPath(sens), ts+','+temp+','+humi);
-		}
-	});
+	// sensor.read(sens.type, sens.pin, function(err, temp, humi) {
+	// 	if (!err) {
+	// 		temp = temp.toFixed(2);
+	// 		humi = humi.toFixed(2);
+	// 		let ts = (new Date()).getTime();
+	// 		currVals[sens.id] = {
+	// 			temp: temp,
+	// 			humi: humi,
+	// 			ts: ts
+	// 		};
+	// 		wss.clients.forEach(function(client) {
+	// 			client.send(currVals);
+	// 		});
+	// 		fs.appendFileSync(dataLogPath(sens), ts+','+temp+','+humi);
+	// 	}
+	// });
 }
 
 
